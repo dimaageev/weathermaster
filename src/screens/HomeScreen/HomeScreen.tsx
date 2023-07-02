96d6f7088110ff8fd3d5980a6ef6ef271aeaeea1
dimaageev <dimitri1ageev@gmail.com>
2023-07-03 00:13:32 +0400

    feat: add cities api call

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,39 @@
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
 
 import styles from "@src/styles";
-import { getCities } from "@src/store/api";
+import { getRandomCity } from "@src/store/api";
+import { City } from "@src/store/types";
 
 const HomeScreen = () => {
+  const [randomCities, setRandomCities] = useState<City[]>();
+
+  console.log(randomCities);
+
   useEffect(() => {
-    getCities();
+    (async () => {
+      const cities: City[] = [];
+      for (let i = 0; i < 3; i++) {
+        const city = await getRandomCity();
+        cities.push(city);
+      }
+      setRandomCities(cities);
+    })();
   }, []);
 
   return (
     <View style={styles.background}>
-      <Text>HomeScreen</Text>
+      <FlatList
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        data={randomCities}
+        renderItem={({ item }) => (
+          <Text>{`${item?.city}, ${item?.country}`}</Text>
+        )}
+      />
     </View>
   );
 };
--- src/store/api.ts
+++ src/store/api.ts
@@ -1,11 +1,40 @@
-const axios = require("axios").default;
+import { City } from "./types";
+import axios from "axios";
 
-export const getCities = () => {
-  axios
-    .get(
-      "http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=10&offset=1&hateoasMode=off"
-    )
-    .then(function (response: any) {
-      console.log(response);
-    });
+function setRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const getRandomCity = () => {
+  return new Promise<City>((resolve, reject) => {
+    let totalPages = 0;
+
+    axios
+      .get(
+        "http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=10&offset=1&hateoasMode=off"
+      )
+      .then((response: any) => {
+        totalPages = Math.ceil(response.data.metadata.totalCount);
+        const pageNumber = setRandomNumber(1, totalPages);
+
+        axios
+          .get(
+            `http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=10&offset=${pageNumber}&hateoasMode=off`
+          )
+          .then((response: any) => {
+            const randomCity =
+              response.data.data[
+                setRandomNumber(0, response.data.data.length - 1)
+              ];
+            console.log(randomCity);
+            resolve(randomCity);
+          })
+          .catch((err: any) => {
+            reject(err);
+          });
+      })
+      .catch((err: any) => {
+        reject(err);
+      });
+  });
 };
